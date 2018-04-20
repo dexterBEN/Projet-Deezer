1393eab23fa748e8e287c40df34bf2c1180f27f3
dexterBEN <33292824+dexterBEN@users.noreply.github.com>
2018-04-20 09:53:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
-import { homePageComponent, albumPageComponent } from './components.js';
+import { homePageComponent, albumPageComponent, artistPageComponent } from './components.js'; //import des différents composants nécessaire au bon fonctionnement de l'application
 
-// console.log(message);
-
-    var routes = [
-        {
-            path:'/',
-            component: homePageComponent
-        },
-        {
-            name: 'albumDescription',
-            path: '/album-page',
-            component: albumPageComponent ,
-            props: true
-        }
-    ];
-
-    var router = new VueRouter({
-        routes
-    });
-
-    var app = new Vue({
-        router,
-
-    }).$mount('#deezer-app')
-
-    export {
-        app
+//Les différentes routes permettant de naviguer entre composants
+var routes = [
+    {
+        path:'/',
+        component: homePageComponent
+    },
+    {
+        name: 'albumDescription',
+        path: '/album-page',
+        component: albumPageComponent ,
+        props: true
+    },
+    {
+        name: 'artistPage',
+        path: '/artist-page',
+        component: artistPageComponent,
+        props: true
     }
+];
+
+var router = new VueRouter({
+    routes
+});
+
+//Instance de vue de l'application
+var app = new Vue({
+    router,
+}).$mount('#deezer-app')
+
+export {
+    app
+}
